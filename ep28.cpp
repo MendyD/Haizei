50843a7269feb3da9febb51dfd2b0d752bcb6ab9
dolai_c@163.com <meng@MengDideMacBook-Pro.local>
2018-12-15 18:34:46 +0800

    第一次更新

---
+++ ep28.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: ep28.cpp
+	> Author: Mendy
+	> Mail: dolai_c@163.com
+	> Course:Number spiral diagonals
+	> Created Time: 五 11/23 20:46:55 2018
+ ************************************************************************/
+
+#include<stdio.h>
+int main(){
+
+    return 0;
+}
